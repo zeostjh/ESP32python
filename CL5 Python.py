97a2a5b88aef0c56e00acb0225d28f0291ef9bd6
zeostjh <greglandon@the-beach.net>
2021-06-15 17:08:19 -0400

    Update CL5 Python.py

--- CL5 Python.py
+++ CL5 Python.py
@@ -8,12 +8,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((host,port))
 
-
 s.sendall("set MIXER:Current/InCh/Fader/On 0 0 0\n".encode())
-
-s.sendall("set MIXER:Current/InCh/Fader/On 1 0 0\n".encode())
-
-s.sendall("set MIXER:Current/InCh/Fader/Level 2 0 0\n".encode())
 
 s.sendall("set MIXER:Current/InCh/Fader/Level 3 0 0\n".encode())
 
