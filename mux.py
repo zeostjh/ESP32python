8c3da67c72d8574155be477975c3f42543c01c1a
zeostjh <greglandon@the-beach.net>
2021-06-15 17:59:45 -0400

    Update mux.py

--- mux.py
+++ mux.py
@@ -1,1 +1,12 @@
 
+from hcsr04 import HCSR04
+import time
+import machine
+
+sensor = HCSR04(trigger_pin=16, echo_pin=0)
+
+while True:
+  distance = sensor.distance_cm()
+  print('Distance:', distance, 'cm')
+  if distance <= 30:
+    
