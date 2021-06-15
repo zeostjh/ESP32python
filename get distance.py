c22285980f03ebfdb36422f0d8e486db432763db
zeostjh <greglandon@the-beach.net>
2021-06-15 17:34:32 -0400

    Update get distance.py

--- get distance.py
+++ get distance.py
@@ -5,3 +5,13 @@ sensor = HCSR04(trigger_pin=16, echo_pin=0)
 distance = sensor.distance_cm()
 
 print('Distance:', distance, 'cm')
+
+
+
+//
+
+distance = sensor.distance_mm()
+
+print('Distance:', distance, 'mm')
+
+//
